eef17d55947ad0be83224a28b546ab0b2361856e
h.bussolari <h.bussolari@quattror.com.br>
2024-10-30 14:39:20 -0300

    Adds Splashscreen to the app + splashscreen config

--- App.tsx
+++ App.tsx
@@ -5,14 +5,20 @@
  * @format
  */
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import type {PropsWithChildren} from 'react';
 import Home from './src/pages/Home';
 import { SafeAreaView, View } from 'react-native';
 import { AppStyles } from './src/styles/styles';
 import TaskProvider from './src/context/TaskContext';
+import SplashScreen from 'react-native-splash-screen';
 
 const App: React.FC = () => {
+
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <TaskProvider>
       <SafeAreaView style={AppStyles.mainContainer}>
--- android/app/src/main/java/com/teste/MainApplication.kt
+++ android/app/src/main/java/com/teste/MainApplication.kt
@@ -10,6 +10,7 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
+import org.devio.rn.splashscreen.SplashScreenReactPackage
 
 class MainApplication : Application(), ReactApplication {
 
@@ -36,7 +37,6 @@ class MainApplication : Application(), ReactApplication {
     super.onCreate()
     SoLoader.init(this, false)
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      // If you opted-in for the New Architecture, we load the native entry point for this app.
       load()
     }
   }
